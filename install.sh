0e38e1690170e2caad1313ced8b37a172859d711
Nicolás E. Ozimica <nozimica@dcc.uchile.cl>
2016-04-07 23:15:30 -0300

    Prepare install.sh to be moved to bin folder.

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@
 # https://github.com/zhimsel/dotfiles/blob/master/install.sh
 
 # Get current dir (so run this script from anywhere)
-
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Strip the last directory (this script resides in bin)
+DOTFILES_DIR="$( realpath ${DOTFILES_DIR}/.. )"
+
 export BACKUP_FOLDER
 BACKUP_FOLDER=backupFolder
 export BACKUP_TBZ
@@ -43,32 +45,32 @@ install_links () {
 
 backup_and_link () {
     local thisFile=$1
-    local currentFile
-    currentFile="$HOME/.$1"
+    local localDotFile
+    localDotFile="$HOME/.$1"
 
     cd $DOTFILES_DIR
 
     # Check if source dotfile exists
-    if [[ ! -e "$thisFile" ]]; then
+    if [[ ! -e "$DOTFILES_DIR/$thisFile" ]]; then
         echo "Error: $thisFile doesn't exist."
         return
     fi
 
-    if [[ ! -e "$currentFile" ]] || [[ -e "$currentFile" ]] && [[ ! -L "$currentFile" ]]; then
-        if [[ -e "$currentFile" ]] && [[ ! -L "$currentFile" ]]; then
+    if [[ ! -e "$localDotFile" ]] || [[ -e "$localDotFile" ]] && [[ ! -L "$localDotFile" ]]; then
+        if [[ -e "$localDotFile" ]] && [[ ! -L "$localDotFile" ]]; then
             echo "Backing up $currentFile."
-            mv $currentFile $BACKUP_FOLDER
+            mv $localDotFile $BACKUP_FOLDER
         fi
-        echo "Creating new $currentFile as a link."
-        ln -s $DOTFILES_DIR/$thisFile $currentFile
+        echo "Creating new $localDotFile as a link."
+        ln -s $DOTFILES_DIR/$thisFile $localDotFile
     else
-        echo "Link for $currentFile already exists."
+        echo "Link for $localDotFile already exists."
     fi
 
     echo ""
 
     if [[ ${commfiles["$thisFile"]} ]]; then
-        echo "Executing post command for $currentFile:"
+        echo "Executing post command for $localDotFile:"
         echo "    ${commfiles[""$thisFile""]}"
         eval ${commfiles["$thisFile"]}
     fi
