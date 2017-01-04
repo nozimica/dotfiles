0829e01c3b4f29e84c6f470fccd3bc676870460b
Nicolás E. Ozimica <nozimica@chucao>
2017-01-04 16:29:13 -0300

    Add colors to update script.

--- bin/updateDotFiles.sh
+++ bin/updateDotFiles.sh
@@ -14,12 +14,6 @@ BACKUP_FOLDER=backupFolder
 export BACKUP_TBZ
 BACKUP_TBZ=$BACKUP_FOLDER-`date +%Y-%m-%d`.tbz
 
-echo "Working into '$DOTFILES_DIR'."
-
-# Update dotfiles itself first
-
-[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
-
 # List of dotfiles
 dfiles=(\
     vimrc \
@@ -65,12 +59,17 @@ backup_and_link () {
         exit 2
     fi
 
-    # test if target file exists and is not a link
-    if [[ -e "$localDotFile" ]] && [[ ! -L "$localDotFile" ]]; then
-        echo "Link for $localDotFile already exists."
+    # test if target file exists
+    if [[ -e "$localDotFile" ]]; then
+        if [[ -L "$localDotFile" ]]; then
+            echo "Link for $localDotFile already exists."
+        else
+            echo "A regular file called $localDotFile already exists."
+        fi
         echo "Backing up $localDotFile."
         cp $localDotFile $localDotFile.origBeforeUpdateDotFiles
-        mv $localDotFile $BACKUP_FOLDER
+        cp $localDotFile $BACKUP_FOLDER
+        rm $localDotFile
     fi
 
     # test if target file does not exist yet
@@ -79,13 +78,13 @@ backup_and_link () {
         ln -s $DOTFILES_DIR/$thisFile $localDotFile
     fi
 
-    echo ""
-
     if [[ ${commfiles["$thisFile"]} ]]; then
         echo "Executing post command for $localDotFile:"
         echo "    ${commfiles[""$thisFile""]}"
         eval ${commfiles["$thisFile"]}
     fi
+
+    echo ""
 }
 
 pack_backup_folder () {
@@ -98,23 +97,31 @@ check_scenario () {
         print_error_msg "Backup folder already exists. Cannot move forward."
         exit 2
     fi
-    if [[ -e "$BACKUP_TBZ" ]]; then
+    echo ""
+    print_msg "Backup folder:"
+    echo $DOTFILES_DIR/$BACKUP_TBZ
+    if [[ -e "$DOTFILES_DIR/$BACKUP_TBZ" ]]; then
         print_error_msg "Backup tbz already exists. Check manually."
         exit 2
     fi
     mkdir $DOTFILES_DIR/$BACKUP_FOLDER
+    echo ""
 }
 
 print_error_msg () {
     echo ""
-    echo " *** ERROR: $1"
+    echo -e "$(tput setaf 1)$(tput bold) *** ERROR: $1$(tput sgr0)"
     echo ""
 }
 
 print_title () {
-    echo "############################################################"
-    echo "#   File: $1"
+    echo -e "$(tput setaf 2)$(tput bold)############################################################$(tput sgr0)"
+    echo -e "$(tput setaf 2)$(tput bold)#   File: $1$(tput sgr0)"
     echo ""
+}
+
+print_msg () {
+    echo -e "$(tput setaf 4)$(tput bold)$1$(tput sgr0)"
 }
 
 echo ""
@@ -122,5 +129,12 @@ echo "For every file in this repo:"
 echo " - we'll backup your current version;"
 echo " - update it with what is here right now."
 echo ""
+
+print_msg "Working into '$DOTFILES_DIR'."
+echo ""
+print_msg "Update dotfiles itself first."
+
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
 check_scenario
 install_links
