2213bb523a35d4bb766523d79db7150e7f83d10f
Nicolás E. Ozimica <nozimica@bancoestado.cl>
2017-11-27 17:52:32 -0300

    Add airline in vim pluggins list.

--- bin/installingVimPluginsPlug.sh
+++ bin/installingVimPluginsPlug.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-# Install vim-plug
+########################################################
+## Setup powerline-fonts
+## Ref: https://powerline.readthedocs.io/en/latest/installation/linux.html#fonts-installation
+[ ! -d ~/var/install/fonts ] && mkdir -p ~/var/install/fonts
+[ ! -d ~/.config/fontconfig/conf.d ] && mkdir -p ~/.config/fontconfig/conf.d
+[ ! -d ~/.fonts ] && mkdir -p ~/.fonts
+
+cd ~/var/install/fonts
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+
+mv PowerlineSymbols.otf ~/.fonts/
+fc-cache -vf
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+########################################################
+## Install vim-plug
 [ ! -d ~/.vim/autoload ] && mkdir -p ~/.vim/autoload
 
 if [[ ! -e ~/.vim/autoload/plug.vim ]]; then
