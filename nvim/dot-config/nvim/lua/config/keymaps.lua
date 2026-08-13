3e6503d30f560daecc9d373e4eab1744ee996f4c
Nicolás E. Ozimica <nozimica@gmail.com>
2026-08-13 02:21:58 +0000

    Add nvim alternative navigation for buffers

--- nvim/dot-config/nvim/lua/config/keymaps.lua
+++ nvim/dot-config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Copy to macOS clipboard (OSC 52) with <leader>y
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
 vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Yank line to system clipboard" })
+
+-- Buffer navigation
+vim.keymap.set("n", "gb", "<cmd>bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "gB", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
