e2b75d29b57c662844d3f1bcf1d1b734e3d1cfe2
Nicolás E. Ozimica <nozimica@gmail.com>
2026-09-24 23:32:22 +0000

    Update nvim colorscheme and lazy extras

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,27 @@
 return {
   -- add themes
   { "ellisonleao/gruvbox.nvim" },
+  { "Rigellute/rigel" },
+  { "shaunsingh/nord.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('nordic').load({})
+    end
+  },
+  {
+    "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+  },
   {
     "neanias/everforest-nvim",
     lazy = false,
@@ -11,25 +32,25 @@ return {
       })
     end,
   },
-  --{
-  --  "maxmx03/solarized.nvim",
-  --  lazy = false,
-  --  priority = 1000,
-  --  ---@type solarized.config
-  --  opts = {},
-  --  config = function(_, opts)
-  --    vim.o.termguicolors = true
-  --    vim.o.background = 'dark'
-  --    require('solarized').setup(opts)
-  --    vim.cmd.colorscheme 'solarized'
-  --  end,
-  --},
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    ---@type solarized.config
+    opts = {},
+    config = function(_, opts)
+      vim.o.termguicolors = true
+      vim.o.background = 'dark'
+      require('solarized').setup(opts)
+      vim.cmd.colorscheme 'solarized'
+    end,
+  },
 
   -- Configure LazyVim to load one of them
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "nord",
     },
   }
 }
