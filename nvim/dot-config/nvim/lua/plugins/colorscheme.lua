91dd408dbb67389f164b23a4866a113dd1fa8568
Nicolás E. Ozimica <nozimica@gmail.com>
2026-09-25 23:13:45 +0000

    Update nvim colorscheme

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,4 @@
 return {
-  -- add themes
   { "ellisonleao/gruvbox.nvim" },
   { "Rigellute/rigel" },
   { "shaunsingh/nord.nvim",
@@ -10,47 +9,26 @@ return {
     'AlexvZyl/nordic.nvim',
     lazy = false,
     priority = 1000,
-    config = function()
-      require('nordic').load({})
-    end
   },
   {
     "catppuccin",
     lazy = false,
     priority = 1000,
     opts = {
-      transparent_background = true,
     },
   },
   {
     "neanias/everforest-nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("everforest").setup({
-        background = "medium",
-      })
-    end,
-  },
-  {
-    "maxmx03/solarized.nvim",
-    lazy = false,
-    priority = 1000,
-    ---@type solarized.config
-    opts = {},
-    config = function(_, opts)
-      vim.o.termguicolors = true
-      vim.o.background = 'dark'
-      require('solarized').setup(opts)
-      vim.cmd.colorscheme 'solarized'
-    end,
   },
 
   -- Configure LazyVim to load one of them
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nord",
+      -- colorscheme = "nordic",
+      colorscheme = "catppuccin-mocha",
     },
   }
 }
